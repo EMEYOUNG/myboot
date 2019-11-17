e2873e74ab11c2e0d7870d7c5b0d2d4b11b07380
Young <Young@qq.com>
2019-11-17 19:31:21 +0800

    双重校验防止缓存击穿，同时加同步锁进行阻塞，保护mysql数据库

--- src/main/java/com/example/controller/UserLoginController.java
+++ src/main/java/com/example/controller/UserLoginController.java
@@ -38,29 +38,39 @@ public class UserLoginController {
     public ModelAndView userLogin(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request, ModelAndView mv){
 
         mv.setViewName("/result");
+        //双重校验防止缓存击穿，同时加同步锁进行阻塞，保护mysql数据库
         Object o = redisUtil.get(username);
-        if(o!=null){
-            User u = (User)o;
-            mv.addObject("user",u);
-            mv.addObject("result","查询缓存成功！");
-            logger.info(username+",查询缓存成功，直接返回！");
-        }else{
-            User user = userService.userLogin(username,password);
-            if(user!=null){
-                mv.addObject("user",user);
-                boolean b = redisUtil.put(username,user,20000);
-                if(b == true){
-                    mv.addObject("result","查询数据库成功并放入redis缓存！");
-                    logger.info(username+",查询数据库成功并放入redis缓存！");
-                }else{
-                    mv.addObject("result","查询数据库成功，放入redis缓存失败！");
-                    logger.info(username+",查询数据库成功，放入redis缓存失败！");
+        if(o==null){
+            synchronized (this){
+                o = redisUtil.get(username);
+                if(o == null){
+                    User user = userService.userLogin(username,password);
+                    if(user!=null){
+                        mv.addObject("user",user);
+                        boolean b = redisUtil.put(username,user,20000);
+                        if(b == true){
+                            mv.addObject("result","查询数据库成功并放入redis缓存！");
+                            logger.info(username+",查询数据库成功并放入redis缓存！");
+                            return mv;
+                        }else{
+                            mv.addObject("result","查询数据库成功，放入redis缓存失败！");
+                            logger.info(username+",查询数据库成功，放入redis缓存失败！");
+                            return mv;
+                        }
+                    }else{
+                        mv.addObject("result","查询数据库失败！");
+                        logger.info(username+",查询缓存和数据库失败！");
+                        return mv;
+
+                    }
+
                 }
-            }else{
-                mv.addObject("result","查询数据库失败！");
-                logger.info(username+",查询缓存和数据库失败！");
             }
         }
+        User u = (User)o;
+        mv.addObject("user",u);
+        mv.addObject("result","查询缓存成功！");
+        logger.info(username+",查询缓存成功，直接返回！");
         return mv;
     }
 
