dc0ec2b929a78a7b6f4941e3ab02ea89ab4a54b0
Young <Young@qq.com>
2019-11-17 20:57:05 +0800

    增加mock模拟实体类测试依赖和DEMO

--- src/test/java/com/example/myboot/MybootApplicationTests.java
+++ src/test/java/com/example/myboot/MybootApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.myboot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,17 @@ public class MybootApplicationTests {
 
     @Test
     public void contextLoads() {
+        //mock一个Iterator类
+        Iterator iterator = mock(Iterator.class);
+        //预设当iterator调用next()时第一次返回hello，第n次都返回world
+        when(iterator.next()).thenReturn("hello").thenReturn("world");
+        //使用mock的对象
+        String result = iterator.next() + " " + iterator.next() + " " + iterator.next();
+        System.out.print(result);
+        //验证结果
+        Assert.assertEquals("hello world world",result);
+
+
     }
 
 }
